304e60ed5c3e8966d02e3f444174576d505f591c
ozodbek <muazzin769698@gmail.com>
2023-06-08 22:13:32 +0900

    5 dars

---
+++ src/App.js
@@ -0,0 +1,180 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div class="row">
+          <div class="col">
+
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Start Bootstrap</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Shoping
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class='svg'> 
+  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-discount" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+   <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+   <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+   <path d="M17 17h-11v-14h-2"></path>
+   <path d="M20 6l-1 7h-13"></path>
+   <path d="M10 10l6 -6"></path>
+   <path d="M10.5 4.5m-.5 0a.5 .5 0 1 0 1 0a.5 .5 0 1 0 -1 0"></path>
+   <path d="M15.5 9.5m-.5 0a.5 .5 0 1 0 1 0a.5 .5 0 1 0 -1 0"><p></p></path>
+   
+</svg>
+
+</div>
+</nav>
+
+
+          </div>
+
+        </div>
+      </header>
+
+      <section>
+     <div className="text">
+      <div class="col1"><h1>Shop in style</h1></div>
+      <div className="col2"><p>With this shop hompeage template</p></div>
+     </div>
+
+     <div class="row rasim">
+
+
+      <div class="col-3">
+        <div class="card">
+         <div className='rang'><p>rasim</p></div>
+         <div class="card-body tekst ">
+         <h5 class="card-title">Fancy Product</h5>
+          <p class="card-text ">$40.00-$80.00</p>
+          <a href="#" class="btn btn-light text-dark border dark">Go somewhere</a>
+         </div>
+        </div>
+        </div>
+
+        <div class="col-3">
+        <div class="card">
+         <div className='rang'><p>rasim</p></div>
+         <div class="card-body tekst ">
+         <h5 class="card-title">Fancy Product</h5>
+          <p class="card-text ">$40.00-$80.00</p>
+          <a href="#" class="btn btn-light text-dark border dark">Go somewhere</a>
+         </div>
+        </div>
+        </div>
+        
+        <div class="col-3">
+        <div class="card">
+         <div className='rang'><p>rasim</p></div>
+         <div class="card-body tekst ">
+         <h5 class="card-title">Fancy Product</h5>
+          <p class="card-text ">$40.00-$80.00</p>
+          <a href="#" class="btn btn-light text-dark border dark">Go somewhere</a>
+         </div>
+        </div>
+        </div>
+
+        <div class="col-3">
+        <div class="card">
+         <div className='rang'><p>rasim</p></div>
+         <div class="card-body tekst ">
+         <h5 class="card-title">Fancy Product</h5>
+          <p class="card-text ">$40.00-$80.00</p>
+          <a href="#" class="btn btn-light text-dark border dark">Go somewhere</a>
+         </div>
+        </div>
+        </div>
+      </div>
+
+      <div class="row rasim">
+
+
+<div class="col-3">
+  <div class="card">
+   <div className='rang'><p>rasim</p></div>
+   <div class="card-body tekst ">
+   <h5 class="card-title">Fancy Product</h5>
+    <p class="card-text ">$40.00-$80.00</p>
+    <a href="#" class="btn btn-light text-dark border dark">Go somewhere</a>
+   </div>
+  </div>
+  </div>
+
+  <div class="col-3">
+  <div class="card">
+   <div className='rang'><p>rasim</p></div>
+   <div class="card-body tekst ">
+   <h5 class="card-title">Fancy Product</h5>
+    <p class="card-text ">$40.00-$80.00</p>
+    <a href="#" class="btn btn-light text-dark border dark">Go somewhere</a>
+   </div>
+  </div>
+  </div>
+  
+  <div class="col-3">
+  <div class="card">
+   <div className='rang'><p>rasim</p></div>
+   <div class="card-body tekst ">
+   <h5 class="card-title">Fancy Product</h5>
+    <p class="card-text ">$40.00-$80.00</p>
+    <a href="#" class="btn btn-light text-dark border dark">Go somewhere</a>
+   </div>
+  </div>
+  </div>
+
+  <div class="col-3">
+  <div class="card">
+   <div className='rang'><p>rasim</p></div>
+   <div class="card-body tekst ">
+   <h5 class="card-title">Fancy Product</h5>
+    <p class="card-text ">$40.00-$80.00</p>
+    <a href="#" class="btn btn-light text-dark border dark">Go somewhere</a>
+   </div>
+  </div>
+  </div>
+</div>
+
+
+
+
+
+      </section>
+
+      <footer>
+        <div className='footer'>
+        <h3>Copyright © Your Website 2023</h3>
+        </div>
+        </footer>
+     
+    
+    </div>
+  );
+}
+
+export default App;
